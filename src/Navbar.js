cb658ff8aab7892f0647ea48f0ff9262528a5424
Ayush Shah <shahayush94@gmail.com>
2024-02-21 01:10:46 +1100

    Applied styling to website

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="navabr">
+    <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
