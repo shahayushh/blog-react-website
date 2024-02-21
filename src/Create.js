66e8f58274974cb50c36d24fcfa3393fdbd6fe15
Ayush Shah <shahayush94@gmail.com>
2024-02-21 23:04:01 +1100

    Created submit functionality for new blogs using fetch

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,30 @@
 import { useState } from "react";
+import {useNavigate} from 'react-router-dom';
 
 const Create= () => {
     const [title, setTitle] = useState('');
     const [body, setBody]=useState('');
     const [author, setAuthor]=useState('mario');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const blog = {title, body, author};
+        fetch('http://localhost:8000/blogs', {
+            method:'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)  
+        }).then( ()=>{
+            navigate('/');
+        })
+
+    
+    }
 
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <label> Blog Title: </label>
                 <input
                 type = "text"
