86a6f46ea487ec0f6f294f48fc3016a25f917491
Ayush Shah <shahayush94@gmail.com>
2024-02-21 15:12:16 +1100

    Applied conditional loading template

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
-
-
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(()=>{
     fetch('http://localhost:8000/blogs')
@@ -12,11 +11,13 @@ const Home = () => {
         return response.json();})
         .then(data=>{
           setBlogs(data);
+          setIsPending(false); //dont display laoding message if fetch has gotten data
         })
   },[]);
 
   return (
     <div className="home">
+      {isPending&& <div> Loading... </div>} 
       {blogs && <BlogList blogs={blogs} title = 'All blogs!'/>}
     </div>
   );
